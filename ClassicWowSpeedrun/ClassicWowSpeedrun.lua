d226d073329a005bf4af6d5650d89e2ec4f20b06
christophrus <christophrus@protonmail.com>
2019-07-09 02:12:29 +0200

    preset selector on first login to new char implemented

--- ClassicWowSpeedrun/ClassicWowSpeedrun.lua
+++ ClassicWowSpeedrun/ClassicWowSpeedrun.lua
@@ -5,12 +5,13 @@ ClassicWowSpeedrunDB = {}
 local g_addonLoaded = false
 local g_playerLoaded = false
 local g_variablesInitialized = false
-local g_firstRun = true
 local g_scrollBarInitialized = false
 local g_guid = false
+local g_runStarted = false
 local g_startDate = false
 local g_initialRealTime = false
 local g_initialPlayedTime = false
+local g_playerInfo = false
 
 local json = LibStub:GetLibrary("LibJSON-1.0", true)
 local checksum = LibStub:GetLibrary("LibChecksum-1.0", true)
@@ -38,14 +39,10 @@ function ClassicWowSpeedrun_OnUpdate()
 	local splits = {}
 	-- first run
 	if (g_addonLoaded and g_playerLoaded and g_variablesInitialized) then
-		splits = ClassicWowSpeedrunDB[g_guid]['splits']
-		if (g_firstRun) then
-			RequestTimePlayed()
-			g_firstRun = false
-		end
+		splits = ClassicWowSpeedrunDB['runs'][g_guid]['splits']
 	end
 
-	if (#splits > 0) then
+	if (#splits > 0) then	
 		g_startDate = splits[1]['realTime']
 		ClassicWowSpeedrunScrollFrame:Show()
 		if (not g_scrollBarInitialized) then
@@ -68,7 +65,7 @@ function ClassicWowSpeedrun_OnUpdate()
 end
 
 function ClassicWowSpeedrunScrollBar_Update()
-	local splits = ClassicWowSpeedrunDB[g_guid]['splits']
+	local splits = ClassicWowSpeedrunDB['runs'][g_guid]['splits']
 	local offset = FauxScrollFrame_GetOffset(ClassicWowSpeedrunScrollFrame)
 	local startDate = splits[1]['realTime']
 	local line
@@ -121,30 +118,63 @@ function ClassicWowSpeedrunScrollBar_Update()
 end
 
 function ClassicSpeedun_UpdateScrollbar()
-	local splits = ClassicWowSpeedrunDB[g_guid]['splits']
+	local splits = ClassicWowSpeedrunDB['runs'][g_guid]['splits']
 	local newOffset = #splits - 8 < 0 and 0 or #splits - 8
 	ClassicWowSpeedrunScrollBar_Update()
 	ClassicWowSpeedrunScrollFrame:SetVerticalScroll(newOffset*36)
 end
 
+local function concatOptions(t, template)
+	local ret = ""
+	local template = template or "%s, "
+	for k, v in pairs(t) do
+		if (v == true) then
+			ret=ret..template:format(k)
+		end
+	end
+	ret, _ = ret:gsub(", $", "")
+	return ret
+end
+
 function ClassicWowSpeedrun_HandlePlayed(playedTime, timePlayedThisLevel, level)
-	local splits = ClassicWowSpeedrunDB[g_guid]['splits']
-	local level = #splits + 1  --UnitLevel("player")
-	local realTime = level > 1 and GetServerTime() or GetServerTime()-playedTime
+	
+	local run = ClassicWowSpeedrunDB['runs'][g_guid]
+	local options = run["options"]
+	local splits = run['splits']
 
-	if (not g_initialPlayedTime and not g_initialRealTime) then
-		g_initialRealTime = GetServerTime()
-		g_initialPlayedTime = playedTime
+	if (#splits > 0 and not g_runStarted) then
+		g_runStarted = true
+		ClassicWowSpeedrun:Show()
 	end
 
-	if (splits[level] == nil) then
-		splits[level] = {
-			['level'] = level,
-			['realTime'] = realTime,
-			['playedTime'] = playedTime
-		}
+	if (playedTime < 30 and not g_runStarted) then
+		ClassicWowSpeedrunNewCharDetected:Show()
+	end 
+
+	if (g_runStarted) then
+		-- set split frame header texts
+		ClassicWowSpeedrunSubTitle:SetText(format("%s - %s", options["preset"], options["difficulty"]))
+		local optionsLabel = concatOptions(options)
+		if (optionsLabel) then
+			ClassicWowSpeedrunSubSubTitle:SetText(format("(%s)", optionsLabel))
+		end
+		local level = #splits + 1  --UnitLevel("player")
+		local realTime = GetServerTime()
+
+		if (not g_initialPlayedTime and not g_initialRealTime) then
+			g_initialRealTime = GetServerTime()
+			g_initialPlayedTime = playedTime
+		end
+
+		if (splits[level] == nil) then
+			splits[level] = {
+				['level'] = level,
+				['realTime'] = realTime,
+				['playedTime'] = playedTime
+			}
+		end
+		ClassicSpeedun_UpdateScrollbar()
 	end
-	ClassicSpeedun_UpdateScrollbar()
 end
 
 function ClassicWowSpeedrun_OnLoad(self)
@@ -179,14 +209,48 @@ end
 function ClassicWowSpeedrun_InitializeVariables()
 	
 	g_guid = UnitGUID("player")
-	if (ClassicWowSpeedrunDB[g_guid] == nil) then
-		ClassicWowSpeedrunDB[g_guid] = {}
+	g_realmName = GetRealmName()
+	g_faction = UnitFactionGroup("player")
+	_, g_name = UnitName("player")
+	_, g_race = UnitRace("player")
+	_, g_class = UnitClass("player")
+
+
+	if (ClassicWowSpeedrunDB['runs'] == nil) then
+		ClassicWowSpeedrunDB['runs'] = {}
 	end
 
-	if (ClassicWowSpeedrunDB[g_guid]['splits'] == nil) then
-		ClassicWowSpeedrunDB[g_guid]['splits'] = {}
+	if ( ClassicWowSpeedrunDB['runs'][g_guid] == nil) then
+
+		ClassicWowSpeedrunDB['runs'][g_guid] = {}
+
+		ClassicWowSpeedrunDB['runs'][g_guid]["realmName"] = g_realmName
+		ClassicWowSpeedrunDB['runs'][g_guid]["name"] = g_name
+		ClassicWowSpeedrunDB['runs'][g_guid]["faction"] = g_faction
+		ClassicWowSpeedrunDB['runs'][g_guid]["race"] = g_race
+		ClassicWowSpeedrunDB['runs'][g_guid]["class"] = g_class
+
+
 	end
 
+	if (ClassicWowSpeedrunDB['runs'][g_guid]['options'] == nil) then
+		ClassicWowSpeedrunDB['runs'][g_guid]['options'] = {}
+	end
+
+	if (ClassicWowSpeedrunDB['runs'][g_guid]['optionsFullfilled'] == nil) then
+		ClassicWowSpeedrunDB['runs'][g_guid]['optionsFullfilled'] = {
+			['noAH'] = true,
+			['noParty'] = true,
+			['noMail'] = true,
+			['noTrade'] = true,
+		}
+	end
+
+	if (ClassicWowSpeedrunDB['runs'][g_guid]['splits'] == nil) then
+		ClassicWowSpeedrunDB['runs'][g_guid]['splits'] = {}
+	end
+
+	RequestTimePlayed()
 	g_variablesInitialized = true
 end
 
@@ -234,4 +298,92 @@ function ClassicWowSpeedrunExportEditBox_OnKeyDown(self, key)
 	if (key == "ESCAPE") then
 		ClassicWowSpeedrunExportFrame:Hide()
 	end
+end
+
+function ClassicWowSpeedrunNotStartRunButton_OnClick()
+	ClassicWowSpeedrunNewCharDetected:Hide()
+end
+
+function ClassicWowSpeedrunStartRunButton_OnClick()
+
+	-- write selected options
+	local options = ClassicWowSpeedrunDB['runs'][g_guid]['options']
+	
+	local subSubLabel = ""
+
+	if (CustomDifficulty:GetChecked()) then
+		options["noAh"] = OptionNoAH:GetChecked()
+		options["noParty"] = OptionNoParty:GetChecked()
+		options["noMail"] = OptionNoMail:GetChecked()
+		options["noTrade"] = OptionNoTrade:GetChecked()
+		options["difficulty"] = "custom difficulty"
+	else
+		options["difficulty"] = "any%"
+	end
+
+	if (OptionTargetFirstTo60:GetChecked()) then
+		options["preset"] = "World #1 to 60"
+	else
+		options["preset"] = format("Target LvL %s", TargetLevelLabel:GetText())
+	end
+
+	if (InCinematic()) then
+		StopCinematic()
+	end
+
+	ClassicWowSpeedrunNewCharDetected:Hide()
+	g_runStarted = true
+	RequestTimePlayed()
+	ClassicWowSpeedrun:Show()
+end
+
+function ClassicWowSpeedrunNewCharDetected_OnShow()
+	if (InCinematic()) then
+		MouseOverrideCinematicDisable(true)
+	end
+	ClassicWowSpeedrunLevelSlider:SetObeyStepOnDrag(true)
+end
+
+function ClassicWowSpeedrunLevelSlider_OnValueChanged(self, value, userInput)
+	TargetLevelLabel:SetText(value)
+end
+
+function CustomDifficulty_OnClick()
+	CustomDifficulty:SetChecked(true)
+	OptionAnyPercent:SetChecked(false)
+	LabelNoAH:Show()
+	OptionNoAH:Show()
+	LabelNoParty:Show()
+	OptionNoParty:Show()
+	LabelNoMail:Show()
+	OptionNoMail:Show()
+	LabelNoTrade:Show()
+	OptionNoTrade:Show()
+end
+
+function OptionAnyPercent_OnClick()
+	CustomDifficulty:SetChecked(false)
+	OptionAnyPercent:SetChecked(true)
+	LabelNoAH:Hide()
+	OptionNoAH:Hide()
+	LabelNoParty:Hide()
+	OptionNoParty:Hide()
+	LabelNoMail:Hide()
+	OptionNoMail:Hide()
+	LabelNoTrade:Hide()
+	OptionNoTrade:Hide()
+end
+
+function OptionTargetCustom_OnClick()
+	OptionTargetCustom:SetChecked(true)
+	OptionTargetFirstTo60:SetChecked(false)
+	ClassicWowSpeedrunLevelSlider:Show()
+	TargetLevelLabel:Show()
+end
+
+function OptionTargetFirstTo60_OnClick()
+	OptionTargetCustom:SetChecked(false)
+	OptionTargetFirstTo60:SetChecked(true)
+	ClassicWowSpeedrunLevelSlider:Hide()
+	TargetLevelLabel:Hide()
 end
